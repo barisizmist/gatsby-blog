3941b135821c9f596255c659dd45acc9ca6a6167
Mert Ocak <mertocak@gmail.com>
2020-05-16 22:05:32 +0300

    new commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL ? 'https://strap-gatsby-app-cms.herokuapp.com' : 'http://localhost:1337',
         queryLimit: 1000, // Default to 100
         contentTypes: [`article`, `user`],
       },
